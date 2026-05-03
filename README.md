93acdce802a817de23ffca311e1614486c332156
tabbeddev <83753407+tabbeddev@users.noreply.github.com>
2026-05-03 14:01:51 +0200

    feat: Add more arguments

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A small program to convert NBS songs to the format for my [Scratch-based MIDI-Ed
 
 ## "Disclaimer"
 
-Technically it can convert Rush E, but the song can't be imported in the Scratch Player, as it hits the max character limit in Ask-Blocks.
+Technically it can convert Rush E, but the song can't be fully imported in the Scratch Player, as it hits the max character limit in Ask-Blocks.
 
 ## Usage
 
@@ -16,3 +16,25 @@ Technically it can convert Rush E, but the song can't be imported in the Scratch
 3. Grab a NBS song (maybe from [Note Block World](https://noteblock.world/))
 4. Convert it: `python main.py path/to/my/song.nbs`
 5. Import the created text file into the Scratch project
+
+## Tips and tricks
+
+### 1. Cut off songs / Song data is too long
+
+You can tell if a song fits fully in the import limit by looking at the last character. If it is an `$` the song will be fully imported.  
+If not it will be cut off somewhere. Either tracks are missing or ending prematurely.
+
+You can sortoff fix this by using the `--limit | -l` argument to cut off the song earlier.
+
+### 2. My song doesn't sound fluent
+
+If the notes aren't held long enough, you can use the `--hold | -o` argument to specify a longer time to hold the note.
+
+### 3. The pitch is too high or low / Some limit has been hit
+
+Use the argument `--transpose | -t` to transpose your song by a given amount of octaves.  
+Keep in mind, that there's an upper and lower limit. The script will try to fix it, when the limit has been hit (a warning will be displayed), but the song might sound off.
+
+### 4. My song is too quiet or loud
+
+Use the argument `--volume | -v` to multiply the volume of the song. `0.5` will half the volume, while `2.0` will double the volume.
--- main.py
+++ main.py
@@ -42,14 +42,14 @@ def get_or_create_first_free_layer(tick: int, instrument: int, volume: int):
     for instr in tracks:
         if (
             instr["instrument"] == get_mapped_instrument(instrument)
-            and instr["volume"] == round(volume / 100 * 13)
+            and instr["volume"] == round(volume / 100 * 13 * volume_factor)
             and (len(instr["notes"]) <= tick or instr["notes"][tick] == " ")
         ):
             return instr
 
     instr: MyInstrument = {
         "instrument": get_mapped_instrument(instrument),
-        "volume": round(volume / 100 * 13),
+        "volume": round(volume / 100 * 13 * volume_factor),
         "muted": False,
         "name": "Converted layer",
         "notes": [],
@@ -62,10 +62,16 @@ def insert_note(instrument: MyInstrument, tick: int, key: int):
     while len(instrument["notes"]) <= tick:
         instrument["notes"].append(" ")
     note = key - 3 + transpose_factor
-    while note >= 84:
-        note -= 12
-    while note < 0:
-        note += 12
+
+    if note > 83:
+        print(f"Upper limit hit with {note} on tick {tick}")
+        while note > 83:
+            note -= 12
+
+    if note < 12:
+        print(f"Lower limit hit with {note} on tick {tick}")
+        while note < 0:
+            note += 12
     instrument["notes"][tick] = NUMBERS_LOOKUP[note]
 
 
@@ -75,17 +81,48 @@ parser = argparse.ArgumentParser(
     prog="./main.py",
     description="A simple converter from nbs files to my scratch music format",
 )
-parser.add_argument("-t", "--transpose", type=int, help="The number to transpose the song by. There's an upper and lower limit")
+parser.add_argument(
+    "-t",
+    "--transpose",
+    type=int,
+    help="The number to transpose the song by. There's an upper (83) and lower (0) limit",
+)
+parser.add_argument(
+    "-o",
+    "--hold",
+    type=float,
+    help="The length of a single note. Defaults to 0.25 scratch beats on tempo 60",
+)
+parser.add_argument(
+    "-l",
+    "--limit",
+    type=int,
+    help="Cut off the song prematurely at tick x. Use when the song wouldn't fit fully in the editor",
+)
+parser.add_argument(
+    "-v",
+    "--volume",
+    type=float,
+    help="Multiplies the volume by this factor. Use when the result is way too loud.",
+)
 parser.add_argument("path", type=Path, help="The nbs file to convert to")
 
 args = parser.parse_args()
 path: Path = args.path
 transpose_factor: int = (args.transpose or 0) * 12
+hold_time: float = args.hold or 0.25
+volume_factor: float = args.volume or 1
+cut_off: int | None = args.limit
 
 file = pynbs.read(path)
 
 for tick, chord in file:
     tick: int = tick
+
+    if cut_off and tick > cut_off:
+        print("Cut off at", tick)
+        break
+
     chord: list[pynbs.Note] = chord
 
     for note in chord:
@@ -94,7 +131,7 @@ for tick, chord in file:
         )
         insert_note(inst, tick, note.key)
 
-output = ""
+output = f"{round(file.header.tempo * 15)}\\{str(hold_time).removeprefix('0')}$"
 for inst in tracks:
     output += inst["name"]
     output += "|"
